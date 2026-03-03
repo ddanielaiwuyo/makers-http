5ebf6b231bd025e4a8755e68e0c77aca03b084e9
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-03-03 11:53:16 +0000

    fixed tests

--- tests/test_artist_repository.py
+++ tests/test_artist_repository.py
@@ -6,16 +6,16 @@ from lib.artist_repository import ArtistRepository
 @fixture
 def expected_result():
     artist_1 = Artist(1, "Pixies", "Rock")
-    artist_1.albums = [Album(1, "Doolittle", "1983-01-01")]
+    artist_1.albums = [Album(1, "Doolittle", "1983-01-01", 1)]
 
     artist_2 = Artist(2, "The Beatles", "Alternative")
-    artist_2.albums = [Album(2, "Abbey Road", "1969-01-01")]
+    artist_2.albums = [Album(2, "Abbey Road", "1969-01-01", 2)]
 
     artist_3 = Artist(3, "Bill Evans", "Jazz")
-    artist_3.albums = [Album(3, "Dig Bill Evans", "2021-05-01")]
+    artist_3.albums = [Album(3, "Dig Bill Evans", "2021-05-01", 3)]
 
     artist_4 = Artist(4, "Phantogram", "Niche")
-    artist_4.albums = [Album(4, "Eyelid Movies", "2010-01-01")]
+    artist_4.albums = [Album(4, "Eyelid Movies", "2010-01-01", 4)]
 
     res = [artist_1, artist_2, artist_3, artist_4]
     return res
--- tests/test_server.py
+++ tests/test_server.py
@@ -12,16 +12,16 @@ from entities.artist import Artist, Album
 @fixture
 def expected_res():
     artist_1 = Artist(1, "Pixies", "Rock")
-    artist_1.albums = [Album(1, "Doolittle", "1983-01-01")]
+    artist_1.albums = [Album(1, "Doolittle", "1983-01-01", 1)]
 
     artist_2 = Artist(2, "The Beatles", "Alternative")
-    artist_2.albums = [Album(2, "Abbey Road", "1969-01-01")]
+    artist_2.albums = [Album(2, "Abbey Road", "1969-01-01", 2)]
 
     artist_3 = Artist(3, "Bill Evans", "Jazz")
-    artist_3.albums = [Album(3, "Dig Bill Evans", "2021-05-01")]
+    artist_3.albums = [Album(3, "Dig Bill Evans", "2021-05-01", 3)]
 
     artist_4 = Artist(4, "Phantogram", "Niche")
-    artist_4.albums = [Album(4, "Eyelid Movies", "2010-01-01")]
+    artist_4.albums = [Album(4, "Eyelid Movies", "2010-01-01", 4)]
 
     res = [artist_1, artist_2, artist_3, artist_4]
     res = [asdict(artist) for artist in res]
@@ -44,5 +44,5 @@ def test_post_artist(web_client, db_conn):
         "/artists",
         query_string={"name": "Mass of the fermenting dreggs", "genre": "Rock"},
     )
-    
+
     assert response.status_code == 201
