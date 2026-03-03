029e2b1f7e7729ccda05b39ac02f31fd193f2107
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-03-03 11:50:04 +0000

    renamed artist file

--- entities/artist.py
+++ entities/artist.py
@@ -1,14 +1,6 @@
 from dataclasses import dataclass, field
 from entities.album import Album
 
-# @dataclass
-# class Album:
-#     """Represents album entity in database"""
-#     id: int
-#     title: str
-#     release_year: str
-
-
 @dataclass
 class Artist:
     """Represents artist entity in database"""
