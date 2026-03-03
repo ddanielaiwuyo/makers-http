e88846ebbf7993dea948c6366afe439f314290c3
ddanielaiwuyo <aiwuyoeghosadaniel2@gmail.com>
2026-03-03 11:33:35 +0000

    removed dead code

--- main.py
+++ main.py
@@ -15,29 +15,9 @@ DB_CONN.seed()
 
 artist_repo = ArtistRepository(DB_CONN)
 
-def handle_post_request(req: Request) -> Response:
-    if req.content_length and req.content_length >= MAX_PAYLOAD:
-        response = jsonify({"status_code": 418, "status": "I am a teapot"})
-        return response
-
-    artist_name = req.args.get("name")
-    genre = req.args.get("genre")
-    
-    if artist_name is None or genre is None:
-        response = jsonify({"status_code": 400, "status": "Bad request"})
-        return response
-
-    artist = Artist(None, artist_name, genre)
-    artist_repo.create(artist)
-    response = jsonify({"status_code": 201, "status": "OK"})
-    response.status_code = 201
-    return response
 
 @app.route("/artists", methods=["GET"])
 def get_artists():
-    # if request.method == "POST":
-    #     return handle_post_request(request)
-
     db_response = artist_repo.get_all()
     db_response = [asdict(artist) for artist in db_response]
     return jsonify(db_response)
